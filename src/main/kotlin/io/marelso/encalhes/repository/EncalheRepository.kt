1716773f1e6f2bbe9b36e3ceede9457da40abcd2
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-08-03 19:43:37 -0300

    Including repository layer to handle encalhes

---
+++ src/main/kotlin/io/marelso/encalhes/repository/EncalheRepository.kt
@@ -0,0 +1,9 @@
+package io.marelso.encalhes.repository
+
+import io.marelso.encalhes.domain.Encalhe
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface EncalheRepository: MongoRepository<Encalhe, String> {
+}
