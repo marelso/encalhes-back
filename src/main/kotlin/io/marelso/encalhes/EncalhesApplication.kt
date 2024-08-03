330c15b3a37710e09c5ee153dd55ef316b672781
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-08-03 18:36:54 -0300

    Initial create

---
+++ src/main/kotlin/io/marelso/encalhes/EncalhesApplication.kt
@@ -0,0 +1,11 @@
+package io.marelso.encalhes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EncalhesApplication
+
+fun main(args: Array<String>) {
+	runApplication<EncalhesApplication>(*args)
+}
---
+++ src/test/kotlin/io/marelso/encalhes/EncalhesApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.marelso.encalhes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EncalhesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
