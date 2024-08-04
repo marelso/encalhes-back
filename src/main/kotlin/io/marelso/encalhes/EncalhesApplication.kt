a752cc8a7ca5da1557b39a564722d4aaf3f51044
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-08-04 09:52:41 -0300

    Overriding auto config

--- src/main/kotlin/io/marelso/encalhes/EncalhesApplication.kt
+++ src/main/kotlin/io/marelso/encalhes/EncalhesApplication.kt
@@ -1,9 +1,12 @@
 package io.marelso.encalhes
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = [MongoReactiveAutoConfiguration::class])
 class EncalhesApplication
 
 fun main(args: Array<String>) {
