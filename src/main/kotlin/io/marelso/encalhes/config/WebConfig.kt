8f63fc87fdde62b0ce40df5ed74bda49d6b6853c
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-08-08 09:46:11 -0300

    Including web config to allow cors

---
+++ src/main/kotlin/io/marelso/encalhes/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package io.marelso.encalhes.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .maxAge(3600L)
+            .allowedHeaders("*")
+            .exposedHeaders("Authorization")
+            .allowCredentials(true)
+    }
+}
