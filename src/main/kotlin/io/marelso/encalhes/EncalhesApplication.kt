b012d5abe8c274ab3e35cb5d511dba4c206c68b5
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-05 09:37:03 -0300

    Allowing schedule

--- src/main/kotlin/io/marelso/encalhes/EncalhesApplication.kt
+++ src/main/kotlin/io/marelso/encalhes/EncalhesApplication.kt
@@ -2,8 +2,10 @@ package io.marelso.encalhes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class EncalhesApplication
 
 fun main(args: Array<String>) {
